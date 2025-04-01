5d7c9cc3dd1da1489a1d9ea89e604623e3d22c81
SebastianJedrak <jedrak.sebastian@gmail.com>
2025-04-01 13:14:03 +0200

    feat: create basic html structure

---
+++ src/components/Input.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input: React.FC = () => {
+  return (
+    <>
+      <label>Label</label>
+      <input type="text" placeholder="Input" />
+    </>
+  );
+};
+
+export default Input;
