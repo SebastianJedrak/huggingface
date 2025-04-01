44ec4cc6524020e5d7c52c5c9f1a49e6ccd57d3c
SebastianJedrak <jedrak.sebastian@gmail.com>
2025-04-01 13:49:21 +0200

    fix: throw error fix

--- src/api/huggingFaceApi.ts
+++ src/api/huggingFaceApi.ts
@@ -1,21 +1,18 @@
 const HUGGING_FACE_URL =
   'https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english';
 
-const getHuggingFaceToken = (): string => {
-  const token = import.meta.env.VITE_HUGGING_FACE_TOKEN;
-  if (!token) {
-    console.warn('API token is not defined in environment variables');
-  }
-  return token as string;
-};
-
 export const fetchHuggingFace = async (text: string = 'test'): Promise<string[] | undefined> => {
   try {
+    const token = import.meta.env.VITE_HUGGING_FACE_TOKEN;
+    if (!token) {
+      throw new Error('Hugging Face API token is not defined');
+    }
+
     const response = await fetch(HUGGING_FACE_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${getHuggingFaceToken()}`,
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({ inputs: text }),
     });
@@ -27,7 +24,8 @@ export const fetchHuggingFace = async (text: string = 'test'): Promise<string[] 
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Error analyzing sentiment:', error);
+    console.error('Fetching data error:', error);
+    throw error;
   }
 };
 
